40712df04375167649ca28ff8903f32312dc872c
Fábio Bortoletto <fabio.bortoletto@hotmail.com>
2023-03-06 22:22:57 -0300

    initial commit

---
+++ src/main/java/org/openrpg/monstercompendium/MonsterCompendiumApplication.java
@@ -0,0 +1,13 @@
+package org.openrpg.monstercompendium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonsterCompendiumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonsterCompendiumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/openrpg/monstercompendium/MonsterCompendiumApplicationTests.java
@@ -0,0 +1,13 @@
+package org.openrpg.monstercompendium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonsterCompendiumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
